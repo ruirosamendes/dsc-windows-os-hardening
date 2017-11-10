f9cf9cd1eaae80f73f3cbbedf7007723861d8cc9
Rui Rosa Mendes <ruirosamendes@users.noreply.github.com>
2017-11-10 16:31:52 +0000

    First Draft

---
+++ measuresconfig.ps1
@@ -0,0 +1,24 @@
+﻿param(
+[string]$computerName,
+[string]$currentDir
+)
+
+Configuration measuresconfig 
+{	
+	Node $computerName
+	{		
+        Registry M889229-disable-ssl-v3
+        {
+            Key = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client'
+            ValueName = 'DisabledByDefault'
+            Ensure = 'Present'            
+            ValueData = '00000001'
+            ValueType = 'Dword'
+        }
+	}
+}
+
+measuresconfig -OutputPath $currentDir\mofs
+
+
+

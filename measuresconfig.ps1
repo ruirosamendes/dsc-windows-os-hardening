aafaf14eae8c4c55979ac6b38bef12927db05b23
Rui Rosa Mendes <ruirosamendes@users.noreply.github.com>
2017-11-10 16:56:36 +0000

    M889229-disable-ssl-v3 New Server Value

--- measuresconfig.ps1
+++ measuresconfig.ps1
@@ -7,12 +7,24 @@ Configuration measuresconfig
 {	
 	Node $computerName
 	{		
-        Registry M889229-disable-ssl-v3
+
+        # BETTER solution to handle registry keys: https://mathieubuisson.github.io/managing-large-numbers-of-registry-settings/
+        #M889229-disable-ssl-v3        
+        Registry key1Value1
         {
             Key = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client'
             ValueName = 'DisabledByDefault'
             Ensure = 'Present'            
             ValueData = '00000001'
+            ValueType = 'Dword'
+        }
+        	
+        Registry key1Value2
+        {
+            Key = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server'
+            ValueName = 'DisabledByDefault'
+            Ensure = 'Present'            
+            ValueData = '00000000'
             ValueType = 'Dword'
         }
 	}
